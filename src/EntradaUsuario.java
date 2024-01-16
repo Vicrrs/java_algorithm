38591ff10a287b23428302cfcbb1d88c613dcfcf
Vicrrs <enricofermi2019@gmail.com>
2024-01-16 00:04:24 -0400

    Entrada do Usuario Scanner

---
+++ src/EntradaUsuario.java
@@ -0,0 +1,14 @@
+package src;
+
+import java.util.Scanner;
+
+public class EntradaUsuario {
+    public static void main(String[] args) {
+        Scanner scanner =  new Scanner(System.in);
+
+        System.out.println("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.println("Olá, " + nome + "!");
+    }
+}
