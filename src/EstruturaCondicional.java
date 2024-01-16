ed29b8ccdc8e23db3347da1c9f5ca9ca58a5432a
Vicrrs <enricofermi2019@gmail.com>
2024-01-15 22:49:23 -0400

    Estrutura Condicional usando `if` para tomar uma decisao

---
+++ src/EstruturaCondicional.java
@@ -0,0 +1,13 @@
+package src;
+
+public class EstruturaCondicional {
+    public static void main(String[] args) {
+        int idade = 19;
+
+        if (idade >= 18) {
+            System.out.println("Você é maior de idade!");
+        } else {
+            System.out.println("Você é menor de idade!");
+        }
+    }
+}
