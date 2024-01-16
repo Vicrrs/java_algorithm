fddceae11b152ececae0c24f727a7690940c80bc
Vicrrs <enricofermi2019@gmail.com>
2024-01-15 23:40:12 -0400

    Funcoes(metodos)

---
+++ src/Funcoes.java
@@ -0,0 +1,12 @@
+package src;
+
+public class Funcoes {
+    public static void main(String[] args) {
+        int resultado = soma(5, 3);
+        System.out.println("Resultado soma: " + resultado);
+    }
+
+    public static int soma(int a, int b) {
+        return a + b;
+    }
+}
