c16d67d6e8e2c54bdcd151351d69c3e9e27011e4
Vicrrs <enricofermi2019@gmail.com>
2024-01-15 23:21:34 -0400

    LoopWhile

---
+++ src/LoopWhile.java
@@ -0,0 +1,12 @@
+package src;
+
+public class LoopWhile {
+    public static void main(String[] args) {
+        int contador = 1;
+
+        while (contador <= 5) {
+            System.out.println("Número: " + contador);
+            contador ++;
+        }
+    }
+}
