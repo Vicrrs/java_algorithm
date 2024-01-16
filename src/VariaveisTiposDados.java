1e618f5e92bcf513172bf52bbd739fae4a407b64
Vicrrs <enricofermi2019@gmail.com>
2024-01-15 22:45:33 -0400

    Variaveis e Tipos de dados

---
+++ src/VariaveisTiposDados.java
@@ -0,0 +1,13 @@
+package src;
+
+public class VariaveisTiposDados {
+    public static void main(String[] args) {
+        int numeroInteiro = 10;
+        double numeroDecimal = 3.14;
+        String texto = "Olá, mundo!";
+
+        System.out.println(numeroInteiro);
+        System.out.println(numeroDecimal);
+        System.out.println(texto);
+    }
+}
