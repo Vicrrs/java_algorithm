32c218ef6df1472f9710d86c3e1ea7e3f4d328cd
Vicrrs <enricofermi2019@gmail.com>
2024-01-16 02:49:43 -0400

    ControlStructures

---
+++ src/ControlStructures.java
@@ -0,0 +1,14 @@
+package src;
+
+public class ControlStructures {
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i + " é par");
+            } else {
+                System.out.println(i + " é ímpar");
+            }
+        }
+    }
+}
+
